a3850d9b17697deed5614b426b53cbbef9a91d47
Albert Montserrat <albert.montserrat@privalia.com>
2018-12-10 16:06:35 +0100

    More

--- RxChallenge2018/AppProviders.swift
+++ RxChallenge2018/AppProviders.swift
@@ -3,19 +3,3 @@ import RxChallengeDomain
 struct AppProviders {
     let typicodeProvider: TypicodeService
 }
-
-//Fake
-import RxSwift
-struct TypicodeServiceFake: TypicodeService {
-    func getPosts() -> Single<[Post]> {
-        return Single.just([])
-    }
-    
-    func getUser(with id: Int) -> Single<User> {
-        return Single.just(User(id: nil, name: nil, username: nil, email: nil, address: nil))
-    }
-    
-    func getComments(for postId: Int) -> Single<[Comment]> {
-        return Single.just([])
-    }
-}
